cd05ce3eed9a32210946bb2d5b69fa231d155f5a
hackyo <137120918@qq.com>
2026-05-29 12:58:48 +0800

    update

--- app/mqtt_client.py
+++ app/mqtt_client.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 import signal
 import threading
@@ -21,8 +20,6 @@ class MqttClient:
         self.on_command = "on"
         self.off_command = "off"
         self.client = None
-        self.loop = asyncio.get_event_loop()
-        self.shutdown_event = threading.Event()
 
     def on_connect(self, _client, _userdata, _flags, reason_code, _properties):
         if reason_code.is_failure:
@@ -37,15 +34,19 @@ class MqttClient:
         else:
             logger.info(f"Subscribing to the topic {MqttConfig.TOPIC} was successful")
 
+    def on_unsubscribe(self, client, _userdata, _mid, _reason_code_list, _properties):
+        logger.info(f"Unsubscribing to the topic {MqttConfig.TOPIC} was successful")
+        client.disconnect()
+
     def on_message(self, _client, _userdata, message):
         msg = message.payload.decode("utf-8")
         if not msg:
             logger.info("Received empty or blank message")
             return
         logger.info(f"Received message with topic {MqttConfig.TOPIC}: {msg}")
-        asyncio.run_coroutine_threadsafe(self.process_message(msg.strip().lower()), self.loop)
+        threading.Thread(target=self.process_message, args=(msg.strip().lower(),), daemon=True).start()
 
-    async def process_message(self, cmd: str):
+    def process_message(self, cmd: str):
         if cmd == self.on_command:
             self.process_power_on()
         elif cmd == self.off_command:
@@ -57,7 +58,7 @@ class MqttClient:
     def process_power_on():
         try:
             logger.info("Start booting up")
-            power_on(WolConfig.ADDRESS, WolConfig.MAC_ADDRESS)
+            power_on(WolConfig.ADDRESS, WolConfig.MAGIC_PACKET)
             logger.info("Bootup successful")
         except Exception as e:
             logger.error(f"Bootup failed: {str(e)}")
@@ -72,20 +73,14 @@ class MqttClient:
         except Exception as e:
             logger.error(f"Shutdown failed: {str(e)}")
 
-    def graceful_shutdown(self, _signum, _frame):
+    def graceful_shutdown(self, _signum=None, _frame=None):
+        logger.info("Shutting down gracefully...")
         if self.client:
-            logger.info(f"Unsubscribe")
             self.client.unsubscribe(MqttConfig.TOPIC)
-            self.client.loop_stop()
-            logger.info(f"Disconnect")
-            self.client.disconnect()
-            self.loop.stop()
-        self.shutdown_event.set()
 
     def init_listener(self):
         signal.signal(signal.SIGINT, self.graceful_shutdown)
         signal.signal(signal.SIGTERM, self.graceful_shutdown)
-        threading.Thread(target=self.loop.run_forever, daemon=True).start()
 
         self.client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2, client_id=MqttConfig.CLIENT_ID,
                                   protocol=mqtt.MQTTv311)
@@ -93,10 +88,9 @@ class MqttClient:
             self.client.tls_set()
         self.client.on_connect = self.on_connect
         self.client.on_subscribe = self.on_subscribe
+        self.client.on_unsubscribe = self.on_unsubscribe
         self.client.on_message = self.on_message
-        self.client.connect(MqttConfig.SERVER, MqttConfig.PORT)
-        try:
-            self.client.loop_forever()
-        except KeyboardInterrupt:
-            self.graceful_shutdown(signal.SIGINT, None)
-        self.shutdown_event.wait()
+
+        self.client.reconnect_delay_set(min_delay=1, max_delay=30)
+        self.client.connect(MqttConfig.SERVER, MqttConfig.PORT, keepalive=30)
+        self.client.loop_forever()
